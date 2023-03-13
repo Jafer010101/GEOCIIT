643cc258312074f84fab697a821562adb83b20a6
Jafer MS <santiago.melqui01@gmail.com>
2023-03-12 20:58:09 -0600

    creacion del sitio geociit

---
+++ geociit1/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import LandingPage from './LandingPage';
+import './styles.css';
+<div className="button-section">
+  <button className="button">Botón 1</button>
+  <button className="button">Botón 2</button>
+  <button className="button">Botón 3</button>
+</div>
+
+function App() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+    
+  )
+  ;
+  
+}
+
+export default App;
+
